c267297323b047cf3e29ea721d19c0206f39680b
neoashley <neoashley06@gmail.com>
2015-09-15 17:41:28 +0000

    cleanup load friends code

--- socialauthapp/views.py
+++ socialauthapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from django.conf import settings
 
 import json
 import oauth2
@@ -10,11 +11,12 @@ def home(request):
                            {'request': request,
                             'user': request.user, })
    if request.user.is_authenticated():
-       #social_user = request.user.social_auth.filter(provider='twitter',).first()
        if request.user.social_auth.filter(provider='twitter',).first():
-           friends = json.loads(oauth_req( 'https://api.twitter.com/1.1/friends/list.json', '294900602-wzPWDkIx6quWgVvoibpNSLYpvv8sOUCl00vr9HbJ', 'xsU46EFIyqd84Rp8f4Lh5HAMVlsMXVnAzL3yEOqDYirae' ))
+           social = request.user.social_auth.get(provider='twitter')
+           friends = json.loads(oauth_req( 'https://api.twitter.com/1.1/friends/list.json', social.extra_data['access_token']['oauth_token'], social.extra_data['access_token']['oauth_token_secret']))
            context['twfriends'] = friends['users']
            return render_to_response("home.html", context_instance = context)
+       # Get google friends: https://python-social-auth.readthedocs.org/en/latest/use_cases.html#retrieve-google-friends
        if  request.user.social_auth.filter(provider='google-oauth2',).first():
            social = request.user.social_auth.get(provider='google-oauth2')
            response = requests.get('https://www.googleapis.com/plus/v1/people/me/people/visible', params={'access_token': social.extra_data['access_token']})
@@ -24,24 +26,8 @@ def home(request):
    return render_to_response('home.html', context_instance=context)
 
 def oauth_req(url, key, secret, http_method="GET", post_body="", http_headers=None):
-    consumer = oauth2.Consumer(key='esLCGMC9I29NljN00nKlm97Qq', secret='nUcjPP7IJ8zKoQoINY1u6VIcW9fq2Cg7s4HT8UwioVdgD5c1FZ')
+    consumer = oauth2.Consumer(key=settings.SOCIAL_AUTH_TWITTER_KEY, secret=settings.SOCIAL_AUTH_TWITTER_SECRET)
     token = oauth2.Token(key=key, secret=secret)
     client = oauth2.Client(consumer, token)
     resp, content = client.request( url, method=http_method, body=post_body, headers=http_headers )
     return content
-    
-#need "sudo pip install oauth2" or "sudo pip easy_install oauth2" 
-def twfriends(request):
-    friends = json.loads(oauth_req( 'https://api.twitter.com/1.1/friends/list.json', '294900602-wzPWDkIx6quWgVvoibpNSLYpvv8sOUCl00vr9HbJ', 'xsU46EFIyqd84Rp8f4Lh5HAMVlsMXVnAzL3yEOqDYirae' ))
-    return render_to_response("home.html",{'twfriends':friends['users'],}) 
-    
-def goofriends(request):
-    social = request.user.social_auth.get(provider='google-oauth2')
-    response = requests.get(
-        'https://www.googleapis.com/plus/v1/people/me/people/visible',
-        params={'access_token': social.extra_data['access_token']}
-    )
-    print(response.json())
-    friends = response.json()['items']
-    return render_to_response("home.html",{'goofriends':friends,}) 
-                            
--- socialauthtest/urls.py
+++ socialauthtest/urls.py
@@ -5,8 +5,6 @@ urlpatterns = patterns('',
     
     url(r'^$', 'socialauthapp.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^goofriends/$', 'socialauthapp.views.goofriends'),
-    url(r'^twfriends/$', 'socialauthapp.views.twfriends'),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url('', include('django.contrib.auth.urls', namespace='auth')),
 )
